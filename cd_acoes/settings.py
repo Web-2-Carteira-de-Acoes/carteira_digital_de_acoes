ac32da483522afe0170ba8da62ff61989971c9de
icaro2222 <icarodias2222@gmail.com>
2022-10-21 22:45:12 -0300

    criando model e form de usuario

--- cd_acoes/settings.py
+++ cd_acoes/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App Interno para o Usuário
+    'usuarios'
 ]
 
 MIDDLEWARE = [
