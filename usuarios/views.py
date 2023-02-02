f9af14904da81e49ce6dff2d8d582154645fdbd5
samy <samaratorquato010@gmail.com>
2023-02-02 16:32:27 -0300

    Estilizando final

--- usuarios/views.py
+++ usuarios/views.py
@@ -54,7 +54,7 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('accounts:user_profile')
+            return redirect('/dashboard')
         else:
             data = {}
             data['msg'] = 'digite uma usuario valido'
