43c5647cc9588c43ae0a2136ebaf1097ab11d393
samy <samaratorquato010@gmail.com>
2023-02-02 08:43:38 -0300

    concertando os links

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,7 +8,6 @@ from . import views
 urlpatterns = [
     path('cadastrar', views.home, name='home'),
     path('login/', views.user_login, name='user_login'),
-    path('logout/', views.user_logout, name='user_logout'),
-    path('profile/', views.user_profile, name='user_profile'),
+    path('logout/', views.user_logout, name='user_logout')
 ]
 
