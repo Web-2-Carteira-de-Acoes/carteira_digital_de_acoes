430d5620631a1425575f3b5b7e094e14cd59fec4
icaro2222 <icarodias2222@gmail.com>
2023-02-11 09:47:28 -0300

    estilizando telas

--- usuarios/views.py
+++ usuarios/views.py
@@ -52,7 +52,7 @@ def user_login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('../dashboard')
+            return redirect('dashboard')
         else:
             data = {}
             data['msg'] = 'digite uma usuario valido'
